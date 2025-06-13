4a142a0176b225abd86ecaf18e2d8f510632ac98
harsh <harshkesharwani777@gmail.com>
2025-06-13 12:12:34 +0530

    loading finetuned weights

--- model.py
+++ model.py
@@ -4,22 +4,28 @@ from decoder import VAE_Decoder
 from diffusion import Diffusion
 
 import model_converter
+import torch
 
-def preload_models_from_standard_weights(ckpt_path, device, finetune_weight_path=None):
+def preload_models_from_standard_weights(ckpt_path, device, finetune_weights_path=None):
     # CatVTON parameters
     in_channels = 9
     out_channels = 4
 
-    state_dict=model_converter.load_from_standard_weights(ckpt_path, device, finetune_weight_path)
+    state_dict=model_converter.load_from_standard_weights(ckpt_path, device)
+
+    diffusion=Diffusion(in_channels=in_channels, out_channels=out_channels).to(device)
+
+    if finetune_weights_path != None:
+        checkpoint = torch.load(finetune_weights_path, map_location=device)
+        diffusion.load_state_dict(checkpoint['diffusion_state_dict'], strict=True)
+    else:
+        diffusion.load_state_dict(state_dict['diffusion'], strict=True)
 
     encoder=VAE_Encoder().to(device)
     encoder.load_state_dict(state_dict['encoder'], strict=True)
 
     decoder=VAE_Decoder().to(device)
     decoder.load_state_dict(state_dict['decoder'], strict=True)
-
-    diffusion=Diffusion(in_channels=in_channels, out_channels=out_channels).to(device)
-    diffusion.load_state_dict(state_dict['diffusion'], strict=False)
 
     clip=CLIP().to(device)
     clip.load_state_dict(state_dict['clip'], strict=True)
